a030e1318b00b1f77bbd6cabc8e9385c19f2372d
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2025-07-23 20:16:43 +0200

    improve error message in the event of a large input

--- src/input_stream/iter.rs
+++ src/input_stream/iter.rs
@@ -1,6 +1,7 @@
 use std::{num::ParseFloatError, str::Utf8Error};
 
 use arrayvec::ArrayVec;
+use bstr::BString;
 
 /// Like the `?` operator, but wraps its output in `Some`
 macro_rules! try_ {
@@ -17,6 +18,12 @@ pub(crate) type ByteIter = Box<dyn Iterator<Item = Result<u8, std::io::Error>>>;
 /// How many digits we attempt to store to parse.
 ///
 /// Numbers represented with more digits than this will cause an error.
+///
+/// This is somewhat arbitrarily chosen as twice as many digits as max integer precision;
+/// at that point, we're already losing a fair amount of precision, but we can still approximate.
+///
+/// The point is that if we get a sequence of bytes that accidentally happens to be megabytes
+/// of ascii digits, we don't attempt to store/parse them all.
 const PARSE_CAPACITY: usize = (f64::DIGITS * 2) as _;
 
 pub struct ParseFloatIterator {
@@ -47,11 +54,17 @@ impl Iterator for ParseFloatIterator {
                 }
             }
 
-            try_!(
-                buffer
-                    .try_push(byte)
-                    .map_err(|_| ParseFloatIteratorError::TooBig)
-            );
+            try_!(buffer.try_push(byte).map_err(|_| {
+                ParseFloatIteratorError::TooBig {
+                    input: BString::new(
+                        buffer
+                            .take()
+                            .into_inner()
+                            .expect("if this were not full we would not be here")
+                            .into(),
+                    ),
+                }
+            }));
         }
 
         if buffer.is_empty() {
@@ -76,8 +89,8 @@ impl Iterator for ParseFloatIterator {
 pub enum ParseFloatIteratorError {
     #[error(transparent)]
     Io(#[from] std::io::Error),
-    #[error("too many digits in input; failed to parse")]
-    TooBig,
+    #[error("too many digits in input; failed to parse \"{input}…\"")]
+    TooBig { input: BString },
     #[error("input was not utf-8")]
     NotUtf8(#[from] Utf8Error),
     #[error("failed to parse \"{input}\"")]
